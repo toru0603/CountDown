79addcca75d63a50c3f832f8daa37c6e416686ae
sekiguchi toru <torutoru0603@hotmail.co.jp>
2014-10-03 22:09:09 +0900

    speak hello world in japanese.

--- CountDown/app/src/main/java/lab/sekiguchi/countdown/CountDown.java
+++ CountDown/app/src/main/java/lab/sekiguchi/countdown/CountDown.java
@@ -3,17 +3,41 @@ package lab.sekiguchi.countdown;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.speech.tts.TextToSpeech;
 import android.view.MenuItem;
+import java.util.Locale;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
 
 
-public class CountDown extends Activity {
+public class CountDown extends Activity implements View.OnClickListener, TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+    private float pitch = 1.0f;
+    private float rate = 1.0f;
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_down);
+
+        startBtn = (Button)findViewById(R.id.start);
+        startBtn.setOnClickListener(this);
+
+        // TTSのインスタンス生成
+        tts = new TextToSpeech(this, this);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(tts != null) {
+            // リソースを解放
+            tts.shutdown();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +57,43 @@ public class CountDown extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("TTS", "onClick");
+        if(v == startBtn) {
+            if(tts != null) {
+                if(tts.setPitch(pitch) == TextToSpeech.ERROR) {
+                    Log.e("TTS", "Ptich(" + pitch + ") set error.");
+                }
+                if(tts.setSpeechRate(rate) == TextToSpeech.ERROR) {
+                    Log.e("TTS", "Speech rate(" + rate + ") set error.");
+                }
+
+                if(tts.isSpeaking()) {
+                    // 読上げ中ならストップ
+                    tts.stop();
+                }
+
+                // テキスト読上げ
+                tts.speak("こんにちわ、世界！", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+        if (status == TextToSpeech.SUCCESS) {
+            Locale locale = Locale.JAPANESE;
+            if (tts.isLanguageAvailable(locale) >= TextToSpeech.LANG_AVAILABLE) {
+                tts.setLanguage(locale);
+            } else {
+                Log.e("TTS", "Not support locale.");
+            }
+        } else {
+            Log.e("TTS", "Init error.");
+        }
+    }
 }
+
